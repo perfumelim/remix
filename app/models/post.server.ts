cd796ef2f1b2250d0a6b073beb1b187369eb7aae
perfumelim <perfumelim@gmail.com>
2022-09-14 18:49:48 +0900

    ~ Action

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,7 @@
 import { prisma } from "~/db.server";
+import type { Post } from "@prisma/client";
 
-export type { Post } from "@prisma/client";
+export type { Post };
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -10,6 +11,8 @@ export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
 }
 
-export async function createPost(post) {
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
   return prisma.post.create({ data: post });
 }
