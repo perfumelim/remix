010a0e5371e2e7285cb0545fa0efc9040b283d08
perfumelim <perfumelim@gmail.com>
2022-09-14 18:36:09 +0900

    ~ Nested Routing

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,19 +1,15 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-// test git config
+export type { Post } from "@prisma/client";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(post) {
+  return prisma.post.create({ data: post });
 }
