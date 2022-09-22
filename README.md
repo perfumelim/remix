b81ade73b0ed7ad7be8a10dfd2db448d6de2fca0
perfumelim <perfumelim@gmail.com>
2022-09-22 15:56:57 +0900

    dialog 태그 연습해보기

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Remix Blog Project 🪄
+
+22.09.22 dialog tag 이용해서 Modal 만들어보기 놀이
---
+++ app/routes/dialog/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Dialog() {
+  const clickOpenModal = () => {
+    const favDialog = document.getElementById("favDialog");
+    favDialog.showModal();
+  };
+  return (
+    <div>
+      <main className="flex w-screen flex-col items-center justify-center pt-40 text-center">
+        <h2 className="text-3xl text-stone-600">Dialog</h2>
+        <p className="mt-16 text-xl">
+          Click buttons below and the dialogs will open in the top layer. <br />
+          Top layer elements will appear on top of any element, including those
+          with the highest z-index.
+        </p>
+        <button onClick={clickOpenModal} className="mt-20">
+          Open Dialog
+        </button>
+      </main>
+      <dialog
+        id="favDialog"
+        className="rounded-md p-16 backdrop:bg-pink-200 backdrop:bg-opacity-50"
+      >
+        <form method="dialog">
+          <p>
+            <label>
+              Favorite animal:
+              <select>
+                <option value="default">Choose…</option>
+                <option>Brine shrimp</option>
+                <option>Red panda</option>
+                <option>Spider monkey</option>
+              </select>
+            </label>
+          </p>
+          <div className="mt-8 flex justify-between">
+            <button value="cancel">Cancel</button>
+            <button id="confirmBtn" value="default">
+              Confirm
+            </button>
+          </div>
+        </form>
+      </dialog>
+    </div>
+  );
+}
+
+export default Dialog;
