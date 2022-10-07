f80746217fafdc53f25b5cbdefbf0aa513f0be56
perfumelim <perfumelim@gmail.com>
2022-10-07 16:53:56 +0900

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,14 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  themes: {
-    colors: {
-      primary: " #f6f5f3",
-      secondary: "#3d3d3f",
-      limegreen: "#7dce94",
-      neutral: "#f9f8fd",
+    extend: {
+      colors: {
+        vanillagrey: " #f6f5f3",
+        darkgrey: "#3d3d3f",
+        limegreen: "#7dce94",
+        whiteish: "#f9f8fd",
+      },
     },
   },
+  plugins: [require("daisyui")],
 };
